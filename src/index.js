0c2ab1580f97a85bb1e1a51a8bc57b07f0f2730d
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-19 23:57:21 -0800

    Added search bar component and youtube api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBCZczP95ntpmWnH9R5dd1gGAQYIdXzbG4';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
